fdc857c09b1a31a880c513608b909a967bdf9eb0
Paul <joonsung218@gmail.com>
2022-04-17 16:06:40 -0500

    Update NFTMarketplace.test.js

--- test/NFTMarketplace.test.js
+++ test/NFTMarketplace.test.js
@@ -34,6 +34,17 @@ contract('NFTMarketplace', (accounts) => {
       const owner_1 = await nft_contract.ownerOf(token_id_1)
       assert.equal(owner_1,accounts[0])
     })
+    it('create 2nd token', async () => {
+      
+      const token_URI_2 = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=1-PUG.json"
+      await nft_contract.createToken(token_URI_2, 'NFT 2',{from:accounts[1]})
+      const token_id_2 = await nft_contract.getLatestId()
+      assert.equal(token_id_2, 2)
+      const requested_uri = await nft_contract.tokenURI(token_id_2)
+      assert.equal(requested_uri,token_URI_2)
+      const owner_2 = await nft_contract.ownerOf(token_id_2)
+      assert.equal(owner_2,accounts[1])
+    })
     it('checking isContract function', async () =>{
         const isContract = await market_contract.isContract(nft_contract.address)
         assert.equal(isContract,true)
@@ -54,6 +65,25 @@ contract('NFTMarketplace', (accounts) => {
         const ownerNew = await nft_contract.ownerOf(1)
         assert.equal(ownerNew,accounts[0])
     })
+    it('checking Lock NFT function two tokens', async () =>{
+      await nft_contract.approve(market_contract.address,1,{from:accounts[0]})
+      await market_contract.lockNFT(nft_contract.address,1,{from:accounts[0]})
+      await nft_contract.approve(market_contract.address,2,{from:accounts[1]})
+      await market_contract.lockNFT(nft_contract.address,2,{from:accounts[1]})
+
+      const owner_1 = await nft_contract.ownerOf(1)
+      assert.equal(owner_1,market_contract.address)
+      const owner_2 = await nft_contract.ownerOf(2)
+      assert.equal(owner_2,market_contract.address)
+
+      await market_contract.unlockNFT(nft_contract.address,1,accounts[0])
+      const ownerNew_1 = await nft_contract.ownerOf(1)
+      assert.equal(ownerNew_1,accounts[0])
+
+      await market_contract.unlockNFT(nft_contract.address,2,accounts[1])
+      const ownerNew_2 = await nft_contract.ownerOf(2)
+      assert.equal(ownerNew_2,accounts[1])
+  })
 
 
   })
